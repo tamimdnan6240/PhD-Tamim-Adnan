05afbc4ef1abe5a4819f780f51080182557f3066
Tamim Adnan <105733511+tamimdnan6240@users.noreply.github.com>
2026-08-02 02:40:46 -0400

    Add files via upload

---
+++ MTUresearch/Agentic-AI-Pavement-Condition-Prediction/Outputs/GPT-4o-mini-Maintenance-2017-2019/final_code.py
@@ -0,0 +1,4 @@
+# Auto-saved discovery script (reproducibility)
+import pandas as pd
+df = pd.read_csv(r'''/content/drive/MyDrive/Agentic-AI-Asset-management/Tamim/codes/Data/Maintenance-data/2017-2019-maintenance-dataset.csv''')
+print(df.head(10))
